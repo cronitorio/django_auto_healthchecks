538412ef99abe8c5b900211409f53c7f29abc54a
Shane Harter <shane@cronitor.io>
2017-02-08 03:04:01 +0000

    Bump version: 0.1.0 → 0.1.1

--- django_auto_healthchecks/__init__.py
+++ django_auto_healthchecks/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import healthchecks
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 url = healthchecks.url
 """ url is a drop-in replacement for django URL that adds a new healthcheck kwarg """
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django_auto_healthchecks',
-    version='0.1.0',
+    version='0.1.1',
     description="Create application health checks directly from your django urls.py",
     long_description=readme + '\n\n' + history,
     author="Cronitor.io",
