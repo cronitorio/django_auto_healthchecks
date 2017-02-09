9c838d2b3379651904eea20bbc56d4c27ddfaf05
Shane Harter <shane@cronitor.io>
2017-02-09 06:05:32 +0000

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django_auto_healthchecks',
-    version='0.1.3',
+    version='0.1.4',
     description="Create application health checks directly from your django urls.py",
     long_description=readme + '\n\n' + history,
     author="Cronitor.io",
