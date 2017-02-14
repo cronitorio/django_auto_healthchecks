e8988d713870ae2680ed67630573e39719603f59
Shane Harter <shane@cronitor.io>
2017-02-14 21:47:37 +0000

    Simplify django 1.10 example

--- examples/django_110/setup.sh
+++ examples/django_110/setup.sh
@@ -5,10 +5,12 @@
 
     pip -V &> /dev/null || echo "ERROR: PIP is Required"
     virtualenv --version &> /dev/null || pip install virtualenv
-    virtualenv -p python3 .virtualenv
+    virtualenv .virtualenv
     source ./.virtualenv/bin/activate
     pip install -r requirements.txt
 
     ## Install the local version of the SDK..
     pip install --editable "file:///$SCRIPT_DIR/../../"
+    unset DJANGO_SETTINGS_MODULE
+    python manage.py migrate
 )
